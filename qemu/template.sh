b9125b95c5a4fbe48456c0faa6a0c2d61f796adb
heliumhydride <yohanleclerre@gmail.com>
2023-08-16 13:28:27 +0200

    add qemu template.sh

---
+++ qemu/template.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+qemu-system-x86_64 -M q35 -accel kvm -m 8G -cpu kvm64 -smp 4 -device qxl-vga -boot menu=on \
+		   -device nec-usb-xhci -device usb-mouse -device usb-kbd \
+		   -netdev user,id=net0 -device virtio-net-pci,netdev=net0 \
+		   -drive file=MYDISKFILE,format=qcow2,if=none,id=hd0,cache=writeback -device virtio-blk,drive=hd0 \
+		   $@
