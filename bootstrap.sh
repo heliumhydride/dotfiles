6d413a41fb9fdcaa0950f93f581cff02b6a8e4e3
heliumhydride <heliumhydride@github.com>
2023-08-28 18:42:08 +0200

    fix linux bootstrapping script

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@
 #     use this at your own risk, it's not my fault if you do something wrong     #
 # -----------------------------------------------------------------------WARNING #
 
-[ "$HOME" = '' ] && ( echo "$HOME is empty, cannot continue."; exit 1; )
+[ -z "$HOME" ] && ( echo "$HOME is empty, cannot continue."; exit 1; )
 
 for i in bashrc vimrc gvimrc xinitrc; do
 	cp -v "_${i}" "$HOME/.${i}"
@@ -16,7 +16,7 @@ cp -rv vimfiles "$HOME/.vim"
 rm -rf "$HOME/scripts"
 cp -rv scripts "$HOME"
 read -rp "qemu directory (default $HOME/test-qemu): " qemu_dir
-rm -rf "$qemu_dir"
-cp -rv qemu "$qemu_dir"
+[ -z "$qemu_dir" ] && qemu_dir="$HOME/test-qemu"
+rm -rf "$qemu_dir"; cp -rv qemu "$qemu_dir"
 
 echo "any changes by machine will have to be made manually!"
