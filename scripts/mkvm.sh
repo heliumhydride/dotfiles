f0945ca9f5e065ce7eee8aad3c33f1aa0c638964
heliumhydride <34401203+heliumhydride@users.noreply.github.com>
2023-08-29 22:16:50 +0200

    add defaults for mkvm script

--- scripts/mkvm.sh
+++ scripts/mkvm.sh
@@ -18,9 +18,12 @@ ask(){
 	read -r "$2"
 }
 
-ask "vm name" VM_NAME
-ask "vm arch (none for default)" VM_ARCH
-ask "storage size" STOR_SIZE
+ask "vm name (def: vm)" VM_NAME
+ask "vm arch (def: x86_64)" VM_ARCH
+ask "storage size (def: 25G)" STOR_SIZE
+[ -z "$VM_NAME" ] && VM_NAME="vm"
+[ -z "$VM_ARCH" ] && VM_ARCH="x86_64"
+[ -z "$STOR_SIZE" ] && STOR_SIZE="25G"
 VM_DIR="$VMS_DIR/$VM_NAME"
 START_FILE="$VM_DIR/start.sh"
 DISK_FILE="$VM_DIR/$VM_NAME.qcow2" 
@@ -34,5 +37,5 @@ mkdir "$VM_DIR" -pv
 cp -v "$TEMPLATE" "$START_FILE"
 
 qemu-img create -f qcow2 "$DISK_FILE" "$STOR_SIZE"
-[ "$VM_ARCH" = "" ] || sed -i "s/x86_64/$VM_ARCH/g" "$START_FILE"
+[ "$VM_ARCH" = "x86_64" ] || sed -i "s/x86_64/$VM_ARCH/g" "$START_FILE"
 sed -i "s/MYDISKFILE/$VM_NAME.qcow2/g" "$START_FILE"
