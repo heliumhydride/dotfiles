311fbd58e80103ac449b15f36d312261da41f0dc
heliumhydride <34401203+heliumhydride@users.noreply.github.com>
2023-08-31 15:28:18 +0200

    make windows bootstrap script behaviour more like the linux one

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -7,10 +7,20 @@ echo _cmdrc.cmd deps: winfetch
 echo modifications to _gvimrc will have to be done manually!
 echo.
 
-mkdir %userprofile%\scripts
+if exist %userprofile%\scripts ( mkdir %userprofile%\scripts )
+else (
+	rd /s /q %userprofile%\scripts
+	mkdir %userprofile%\scripts
+)
 xcopy scripts-win32 %userprofile%\scripts\
-mkdir %userprofile%\vimfiles
+
+if exist %userprofile%\vimfiles ( mkdir %userprofile%\vimfiles )
+else (
+	rd /s /q %userprofile%\vimfiles
+	mkdir %userprofile%\vimfiles
+)
 xcopy vimfiles %userprofile%\vimfiles\ /s
+
 echo.
 
 for %%i in (regfiles\*) do (
