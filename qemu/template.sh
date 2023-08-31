954320aaae54af25a0417fc5965402bdb969ad52
heliumhydride <34401203+heliumhydride@users.noreply.github.com>
2023-08-31 16:30:59 +0200

    qemu/template: remove zoom to fit by default, add qemu monitor to stdio

--- qemu/template.sh
+++ qemu/template.sh
@@ -4,5 +4,5 @@ qemu-system-x86_64 -M q35 -accel kvm -m 8G -cpu kvm64 -smp 4 -device qxl-vga -bo
 		   -device nec-usb-xhci -device usb-mouse -device usb-kbd \
 		   -netdev bridge,br=virbr0,id=net0 -device virtio-net-pci,netdev=net0 \
 		   -drive file=MYDISKFILE,format=qcow2,if=none,id=hd0,cache=writeback -device virtio-blk,drive=hd0 \
-		   -display gtk,zoom-to-fit=on -rtc base=utc -audiodev pa,id=snd0 -device intel-hda,id=snd0 \
+		   -monitor stdio -rtc base=utc -audiodev pa,id=snd0 -device intel-hda,id=snd0 \
 		   $@
