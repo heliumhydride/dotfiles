793824d011cb1d136065c26ad95a94590a065c5c
heliumhydride <34401203+heliumhydride@users.noreply.github.com>
2024-03-12 22:50:16 +0100

    fix this awful bootstrap.cmd and chmod +x a script

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -1,31 +1,31 @@
 @echo off
 
-copy _cmdrc.cmd %userprofile%
-copy _vimrc-win32 %userprofile%\_vimrc
-copy _gvimrc %userprofile%
+copy _cmdrc.cmd "%userprofile%"
+copy _vimrc-win32 "%userprofile%\_vimrc"
+copy _gvimrc "%userprofile%"
 echo _cmdrc.cmd deps: winfetch
 echo modifications to _gvimrc will have to be done manually!
 echo.
 
-if exist %userprofile%\scripts ( mkdir %userprofile%\scripts )
+if not exist "%userprofile%\scripts" ( mkdir "%userprofile%\scripts" )
 else (
-	rd /s /q %userprofile%\scripts
-	mkdir %userprofile%\scripts
+	rd /s /q "%userprofile%\scripts"
+	mkdir "%userprofile%\scripts"
 )
-xcopy scripts-win32 %userprofile%\scripts\
+xcopy scripts-win32 "%userprofile%\scripts\"
 
-if exist %userprofile%\vimfiles ( mkdir %userprofile%\vimfiles )
+if not exist "%userprofile%\vimfiles" ( mkdir "%userprofile%\vimfiles" )
 else (
-	rd /s /q %userprofile%\vimfiles
-	mkdir %userprofile%\vimfiles
+	rd /s /q "%userprofile%\vimfiles"
+	mkdir "%userprofile%\vimfiles"
 )
-xcopy vimfiles %userprofile%\vimfiles\ /s
+xcopy vimfiles "%userprofile%\vimfiles\" /s
 
 echo.
 
 for %%i in (regfiles\*) do (
 	reg import %%i
 )
-e
+
 echo.
 echo rmskins will have to be installed manually!
