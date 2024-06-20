cd4e8777c513d9a4e950ad298847cbd6cff47860
heliumhydride <34401203+heliumhydride@users.noreply.github.com>
2024-06-20 21:02:20 +0200

    _cmdrc.cmd: make prompt more similar to bashrc

--- _cmdrc.cmd
+++ _cmdrc.cmd
@@ -2,5 +2,5 @@
 set MUSIC_PATH=D:\music
 cls
 fastfetch
-prompt $_[$P] [$T]$_$G 
+prompt $_[$E[1;32m%username%$e[0m@$E[1;32m%computername%$E[0m] [$E[1;34m$P$E[0m] [$E[1;35m$T$E[0m]$_$G 
 "C:\Program Files (x86)\Clink\clink_x64.exe" inject
