ff39666d115e60d67ea557c1f430da154f15d318
heliumhydride <34401203+heliumhydride@users.noreply.github.com>
2024-07-19 16:54:42 +0200

    _cmdrc.cmd: make prompt user and hostname color red if admin (akin to bashrc)

--- _cmdrc.cmd
+++ _cmdrc.cmd
@@ -2,5 +2,10 @@
 set MUSIC_PATH=D:\music
 cls
 fastfetch
-prompt $_[$E[1;32m%username%$e[0m@$E[1;32m%computername%$E[0m] [$E[1;34m$P$E[0m] [$E[1;35m$T$E[0m]$_$G 
+net session >nul 2>&1
+if %errorlevel% == 0 (
+  prompt $_[$E[1;31m%username%$e[0m@$E[1;31m%computername%$E[0m] [$E[1;34m$P$E[0m] [$E[1;35m$T$E[0m]$_$G 
+) else (
+  prompt $_[$E[1;32m%username%$e[0m@$E[1;32m%computername%$E[0m] [$E[1;34m$P$E[0m] [$E[1;35m$T$E[0m]$_$G 
+)
 "C:\Program Files (x86)\Clink\clink_x64.exe" inject
