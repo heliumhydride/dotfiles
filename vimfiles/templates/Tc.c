5019b8409324e4276bd964d67e02cb03c6778fda
heliumhydride <34401203+heliumhydride@users.noreply.github.com>
2024-08-25 18:40:45 +0200

    make C template more complete

--- vimfiles/templates/Tc.c
+++ vimfiles/templates/Tc.c
@@ -1,5 +1,10 @@
+/// Useful includes
 #include <stdio.h>
+//#include <stdlib.h>
+//#include <getopt.h>
+//#include <unistd.h>
+//#include <string.h>
 
-int main(){
+int main(int argc, char* argv[]){
 	return 0;
 }
